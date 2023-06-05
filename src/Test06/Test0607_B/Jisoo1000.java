8f977f5b0d9b2b4b6594ab81086651f3fef6fd2d
JisooPyo <audwl326@gmail.com>
2023-06-05 12:14:27 +0900

    Jisoo_0607_B
    Jisoo solved BOJ 1000,2558

--- src/Test06/Test0607_B/Jisoo1000.java
+++ src/Test06/Test0607_B/Jisoo1000.java
@@ -1,6 +1,13 @@
 package Test06.Test0607_B;
+
+import java.util.Scanner;
+// 백준에서 import 안했더니 한 번 틀렸다!
+
 public class Jisoo1000 {
     public static void main( String[] args ) {
-
+        Scanner sc = new Scanner( System.in );
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println( a + b );
     }
 }
