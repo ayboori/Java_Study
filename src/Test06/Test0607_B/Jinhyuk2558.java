18a6e3c13f4b714b8a63e3a78cb8f66e36a788c4
jin <132440453+05030522@users.noreply.github.com>
2023-06-07 12:55:54 +0900

    jinhyuk_B_1000,2558

--- src/Test06/Test0607_B/Jinhyuk2558.java
+++ src/Test06/Test0607_B/Jinhyuk2558.java
@@ -1,6 +1,13 @@
 package Test06.Test0607_B;
+
+import java.util.Scanner;
 public class Jinhyuk2558 {
     public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt(); //int인 a값을 받아옴
+        int b = sc.nextInt(); //int인 b값을 받아옴
+
+        System.out.println(a+b); //a+b값을 출력
     }
 }
