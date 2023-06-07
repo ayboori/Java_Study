347e6a831b094c78a91f89907b5295bada9bc922
ayoung <ayoung3052@gmail.com>
2023-06-07 14:31:07 +0900

    Ayong_B_1000,2558

--- src/Test06/Test0607_B/Ayoung1000.java
+++ src/Test06/Test0607_B/Ayoung1000.java
@@ -1,7 +1,13 @@
 package Test06.Test0607_B;
 
+import java.util.Scanner;
+
 public class Ayoung1000 {
     public static void main( String[] args ) {
-
+    	Scanner scanner = new Scanner(System.in);
+    	int a = scanner.nextInt();
+    	int b = scanner.nextInt();
+    	System.out.println(a+b);
+    	scanner.close();
     }
 }
