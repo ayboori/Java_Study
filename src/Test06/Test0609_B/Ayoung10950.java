8fb7ecbbc5c867d7a32bdcd63e9c8893f0cd1e39
ayoung <ayoung3052@gmail.com>
2023-06-08 17:02:41 +0900

    ayoung_B_10950,10951

--- src/Test06/Test0609_B/Ayoung10950.java
+++ src/Test06/Test0609_B/Ayoung10950.java
@@ -1,7 +1,14 @@
 package Test06.Test0609_B;
 
+import java.util.Scanner;
+
 public class Ayoung10950 {
     public static void main( String[] args ) {
-
+    	Scanner scanner = new Scanner(System.in);
+    	while(scanner.hasNextInt()) {
+        	int firstNum = scanner.nextInt();
+        	int secondNum = scanner.nextInt();
+        	System.out.println(firstNum + secondNum);
+    	}
     }
 }
