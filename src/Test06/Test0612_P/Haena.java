1f8d3463c32414035f7558a68a998300f3f6b861
JisooPyo <audwl326@gmail.com>
2023-06-09 16:24:21 +0900

    Hyena.java를 Haena.java로 이름 변경

--- src/Test06/Test0612_P/Haena.java
+++ src/Test06/Test0612_P/Haena.java
@@ -2,10 +2,10 @@
 
 package Test06.Test0612_P;
 
-public class Hyena {
+public class Haena {
     // 예시를 이용하여 solution 메서드를 잘 구현했는지 확인하는 main 메서드
     public static void main( String[] args ) {
-        Hyena hn = new Hyena();
+        Haena hn = new Haena();
 
         // 프로그래머스 문제에서 나오는 예제들
         String t1 = "1 2 3 4";
