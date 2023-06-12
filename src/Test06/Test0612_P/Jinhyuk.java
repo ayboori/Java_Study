7cd295d3668d99c9cfe4a335e8c21bccf79f10d9
jin <132440453+05030522@users.noreply.github.com>
2023-06-12 15:26:44 +0900

    jinhyuk

--- src/Test06/Test0612_P/Jinhyuk.java
+++ src/Test06/Test0612_P/Jinhyuk.java
@@ -1,27 +1,36 @@
 // 최댓값과 최솟값
 
 package Test06.Test0612_P;
-
+import java.util.Scanner;
 public class Jinhyuk {
-    // 예시를 이용하여 solution 메서드를 잘 구현했는지 확인하는 main 메서드
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Jinhyuk jh = new Jinhyuk();
-
-        // 프로그래머스 문제에서 나오는 예제들
-        String t1 = "1 2 3 4";
-        String t2 = "-1 -2 -3 -4";
-        String t3 = "-1 -1";
-
-        System.out.println( jh.solution( t1 ) );
-        System.out.println( jh.solution( t2 ) );
-        System.out.println( jh.solution( t3 ) );
-
+        System.out.println(Jinhyuk.Solution);
     }
+    // 예시를 이용하여 solution 메서드를 잘 구현했는지 확인하는 main 메서드
+    class Solution {
+        public String solution(String s) {
+            Scanner in = new Scanner(System.in);
 
-    // solution 메서드
-    public String solution( String s ) {
-        String answer = "";
-        // 문제 푸는 부분
-        return answer;
+            int a = in.nextInt();
+            int[] list = new int[a];
+
+            for (int i = 0; i < a; i++) {
+                list[i] = in.nextInt();
+            }
+            in.close();
+            int max = list[0];
+            int min = list[0];
+            for (int i = 0; i < list.length; i++) {
+                if (list[i] > max) {
+                    max = list[i];
+                }
+                if (list[i] < min) {
+                    min = list[i];
+                }
+            }
+            String answer = min + " " + max;
+            return answer;
+        }
     }
 }
