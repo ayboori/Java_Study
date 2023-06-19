21f213d45af1aac2d3f5b1f554b5d8c9fd26b369
JisooPyo <audwl326@gmail.com>
2023-06-19 23:06:20 +0900

    Jisoo_P_약수의 개수와 덧셈
    제곱수는 약수의 개수가 홀수임을 이용해서 풀었습니다.
    다른 사람의 풀이는 저와 로직이 비슷하거나, for문을 돌려 약수의 개수를 찾아 낸 후 더해 주거나 빼주는 방식이라서가져오지 않았습니다.

--- src/Test06/Test0621_P/Jisoo.java
+++ src/Test06/Test0621_P/Jisoo.java
@@ -2,4 +2,34 @@
 package Test06.Test0621_P;
 
 public class Jisoo {
+    public static void main( String[] args ) {
+        Jisoo jisoo = new Jisoo();
+
+        System.out.println( jisoo.solution( 13, 17 ) );
+        System.out.println( jisoo.solution( 24, 27 ) );
+
+    }
+
+    public int solution( int left, int right ) {
+        int answer = 0;
+        for ( int i = left; i <= right; i++ ) {
+            answer += isSquareNumber( i ) ? ( -1 ) * i : i;
+        }
+        return answer;
+    }
+
+    public boolean isSquareNumber( int num ) {
+        return ( (int) Math.sqrt( num ) ) * ( (int) Math.sqrt( num ) ) == num ? true : false;
+    }
+
 }
+
+/*
+테스트 1 〉	통과 (0.11ms, 78.3MB)
+테스트 2 〉	통과 (0.07ms, 75.6MB)
+테스트 3 〉	통과 (0.07ms, 70.8MB)
+테스트 4 〉	통과 (0.03ms, 77.5MB)
+테스트 5 〉	통과 (0.16ms, 77.8MB)
+테스트 6 〉	통과 (0.03ms, 76.6MB)
+테스트 7 〉	통과 (0.04ms, 78.8MB)
+ */
