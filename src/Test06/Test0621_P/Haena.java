a5736d06a08ee35be95af2110997dafb1f282b04
Haena,Cho <131599243+HaenaCho01@users.noreply.github.com>
2023-06-21 20:36:40 +0900

    Haena_P_약수의 개수와 덧셈 (#37)
    * Haena_P_약수의 개수와 덧셈
    
    * 풀이 과정
    1. 숫자와 약수의 개수를 하나의 묶음으로 생각하여 left~right 숫자의 개수만큼 2차원 배열을 생성한다.
    2. for문을 통해 모든 숫자를 돌아가며 하나의 배열 중 앞에는 각 숫자를, 뒤에는 약수를 구해 넣는다.
    3. 2차원배열을 돌면서 약수의 개수가 짝수인 경우 숫자를 +하고, 약수의 개수가 홀수인 경우 숫자를 -하도록 한다.

--- src/Test06/Test0621_P/Haena.java
+++ src/Test06/Test0621_P/Haena.java
@@ -2,4 +2,60 @@
 package Test06.Test0621_P;
 
 public class Haena {
+    public static void main(String[] args) {
+        SolutionHn sol = new SolutionHn();
+
+        System.out.println(sol.solution(13, 17));
+        System.out.println(sol.solution(24, 27));
+        System.out.println(sol.solution(2, 9));
+    }
 }
+
+class SolutionHn {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        // 2차배열 {숫자, 약수개수},...
+        int[][] arr = new int[(right-left+1)][2];
+
+        for (int i = 0; i < arr.length; i++) {
+            // 하나의 배열 중 앞에 숫자 넣기
+            arr[i][0] = left + i;
+            int num = arr[i][0];
+
+            // 하나의 배열 중 뒤에 약수 구해 넣기
+            for (int j = 1; j <= num; j++) {
+                if (num % j == 0) {
+                    arr[i][1]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i][0]; // 숫자
+            int divisorCnt = arr[i][1]; // 약수의 개수
+
+            // 약수의 개수가 짝수인 경우 +
+            if (divisorCnt % 2 == 0) {
+                answer  = answer + num;
+            }
+
+            // 약수의 개수가 홀수인 경우 -
+            else if (divisorCnt % 2 == 1) {
+                answer = answer - num;
+            }
+        }
+
+        return answer;
+    }
+}
+
+/*
+테스트 1 〉	통과 (5.12ms, 73.4MB)
+테스트 2 〉	통과 (1.82ms, 75MB)
+테스트 3 〉	통과 (1.39ms, 77.2MB)
+테스트 4 〉	통과 (0.55ms, 79.5MB)
+테스트 5 〉	통과 (4.05ms, 80.6MB)
+테스트 6 〉	통과 (0.40ms, 77.2MB)
+테스트 7 〉	통과 (0.17ms, 73.7MB)
+ */
