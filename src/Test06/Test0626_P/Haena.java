8e94b34b33cd5c147f9246cc68230c60b7341f7f
Haena,Cho <131599243+HaenaCho01@users.noreply.github.com>
2023-06-26 19:55:44 +0900

    Haena_P_최대공약수와 최소공배수 (#50)
    * Haena_P_최대공약수와 최소공배수
    
    * 풀이 과정
    1. 최대공약수 구하기
     1-1) 주어진 두 값중 작은 값을 Math.min 함수를 통해 찾는다.
     1-2) for문을 통해 작은 값부터 1까지 줄어들며 최대공약수를 찾아 반환한다.
    2. 최소공배수 구하기
     2-1) 주어진 두 값을 곱한 후 최대공약수로 나눠 최소공배수를 찾아 반환하기
     => 처음에 for문 을 통해 최소공배수를 찾아 반환했다가 시간이 너무 오래 걸려 찾아보던 중 최소공배수에 대해 다시 공부해 해당 내용으로 수정함
    
    * 테스트 통과 시간 : 0.02ms ~ 0.10ms <- 0.23ms ~ 7.77ms

--- src/Test06/Test0626_P/Haena.java
+++ src/Test06/Test0626_P/Haena.java
@@ -1,5 +1,53 @@
 // lv. 1 최대공약수와 최소공배수
 package Test06.Test0626_P;
 
+import java.util.Arrays;
+
 public class Haena {
+    public static void main(String[] args) {
+        SolutionHn sol = new SolutionHn();
+        System.out.println(Arrays.toString(sol.solution(3, 12)));
+        System.out.println(Arrays.toString(sol.solution(2, 5)));
+        System.out.println(Arrays.toString(sol.solution(4, 20)));
+        System.out.println(Arrays.toString(sol.solution(3, 7)));
+    }
+
 }
+
+class SolutionHn {
+    public int[] solution(int n, int m) {
+        // 최대공약수 구하기
+        int g = 0;
+        int min = Math.min(n, m);
+        for (int i = min; i >= 1; i--) {
+            if (n % i == 0 && m % i == 0) {
+                g = i;
+                break;
+            }
+        }
+
+        // 최소공배수 구하기
+        int l = m * n / g;
+
+        return new int[] {g, l};
+    }
+}
+
+/* 0.02ms ~ 0.10ms
+테스트 1 〉	통과 (0.02ms, 78.2MB)
+테스트 2 〉	통과 (0.03ms, 76.6MB)
+테스트 3 〉	통과 (0.05ms, 77.7MB)
+테스트 4 〉	통과 (0.03ms, 76.8MB)
+테스트 5 〉	통과 (0.02ms, 71.1MB)
+테스트 6 〉	통과 (0.04ms, 77.1MB)
+테스트 7 〉	통과 (0.04ms, 72MB)
+테스트 8 〉	통과 (0.03ms, 74.1MB)
+테스트 9 〉	통과 (0.04ms, 77.9MB)
+테스트 10 〉	통과 (0.03ms, 76.1MB)
+테스트 11 〉	통과 (0.04ms, 78.6MB)
+테스트 12 〉	통과 (0.10ms, 76.8MB)
+테스트 13 〉	통과 (0.05ms, 71.3MB)
+테스트 14 〉	통과 (0.05ms, 72.7MB)
+테스트 15 〉	통과 (0.03ms, 73.5MB)
+테스트 16 〉	통과 (0.05ms, 77.2MB)
+ */
