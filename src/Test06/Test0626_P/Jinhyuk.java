9203900a9eae267602d9e40aa3db9d2b5f2c1d9b
jin <132440453+05030522@users.noreply.github.com>
2023-06-26 19:56:59 +0900

    Jinhyuk_P_최대공약수와 최소공배수 (#51)

--- src/Test06/Test0626_P/Jinhyuk.java
+++ src/Test06/Test0626_P/Jinhyuk.java
@@ -2,4 +2,30 @@
 package Test06.Test0626_P;
 
 public class Jinhyuk {
+    public static void main(String[] args) {
+        Jinhyuk jinhyuk = new Jinhyuk();
+        int n1 = 3;
+        int m1 = 12;
+
+        int n2 = 2;
+        int m2 = 5;
+
+        System.out.println(jinhyuk.solution(n1, m1));
+        System.out.println(jinhyuk.solution(n2, m2));
+    }
+
+    public int[] solution(int n, int m) {
+        int lcm = 0;
+        int gcd = 0;
+
+        for (int i = 1; i <= m && i <= n; i++) {
+            if (n % i == 0 && m % i == 0) {
+                gcd = i;
+            }
+        }
+        lcm = (m*n)/gcd;
+
+        int[] answer = {gcd, lcm};
+        return answer;
+    }
 }
