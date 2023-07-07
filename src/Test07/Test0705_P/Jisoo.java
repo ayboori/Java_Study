3afd6d30b4ed9a945e74805f355e8355493f054f
JisooPyo <130378232+JisooPyo@users.noreply.github.com>
2023-07-07 17:19:21 +0900

    Jisoo_P_이상한 문자 만들기, 예산 (#65)
    * Jisoo_P_이상한 문자 만들기
    
    * 첫 번째 풀이
    1. 주어진 문자열 s를 소문자로 바꾸고 StringBuilder의 생성자에 넣어 초기화해준다.
    2. StringBuilder의 charAt(i)를 for문을 돌면서 조사한다.
    3. 공백이면 boolean flag = false, 공백이 아니면 boolean flag = true, index = 0으로 정해준다.
    4. flag가 true이면 index가 짝수인지 조사하고 짝수이면 대문자로 바꾼다. index++해준다.
    5. 마지막에 StringBuiler.toString()을 이용하여 return해준다.
    
    * 다른 사람의 풀이
    -> 주어진 문자열 s를 s.toCharArray()를 이용하여 char[]을 만들어 수정하였다.
    -> flag를 정하지 않고 공백이냐 아니냐로 나누었다.
    -> idx++ % 2 == 0 의 조건과 삼항연산자를 이용하여 코드가 짧다!
    -> String.valueOf(char 배열) 메서드를 이용해 return하였다.
    
    * 다른 사람의 풀이 2
    -> 주어진 문자열 s를 s.split("")을 이용하여 String[]을 만들어 수정하였다.
    -> 배열안의 원소가 공백인지 아닌지 contains(" ") 메서드를 이용하여 확인한 후 진행하였다.
    -> String answer = ""에 concat을 하여 return하였다.
    -> 짧아서 좋아요는 많이 받았지만 돌려보니 느렸다.
    ==> 원인 분석을 해보고 코드를 개선하였다.
    
    * Jisoo_Test
    문자열이 주어지는데 문자열을 수정해야 할 때, char[]로 만들어 수정하는 것이 더 빠를까 StringBuilder로 초기화하여 setCharAt() 메서드를 이용하여 수정하는 것이 더 빠를까 궁금해져서 Test해봄!
    
    * 통과 시간 추가
    
    * Jisoo_P_예산
    
    1. 첫번째 풀이
    배열을 정렬해서 작은것부터 넣으면된다! 넘치면 반환!
    
    2. 두번째 풀이
    처음에 이 조건이라고 생각해서 너무 어렵게 풀었다.
    예산 내에 쓰는 것이 아니라 예산에 꼭 맞춰서 쓰는 것으로 생각해서 한참을
    풀었다. ㅠㅠ 그래도 일단 풀긴 했고 생각보다 코드는 짧아서 공유해봄..
    발표 자료 준비해려 했으나 프로젝트가 생각보다 빡세서 준비할 시가니가
    없어따.. 아 이런 풀이도 있네..? 정도 생각해주시면 될 것 같다.

--- src/Test07/Test0705_P/Jisoo.java
+++ src/Test07/Test0705_P/Jisoo.java
@@ -1,4 +1,68 @@
 package Test07.Test0705_P;
 
 public class Jisoo {
+	public static void main(String[] args) {
+		Jisoo jisoo = new Jisoo();
+		System.out.println(jisoo.solution("try hello world"));
+	}
+
+	public String solution(String s) {
+		// s를 소문자로 바꾸고 StringBuilder로 초기화.
+		// setCharAt() 메서드를 사용하기 위하여
+		StringBuilder sb = new StringBuilder(s.toLowerCase());
+
+		// 단어를 바꾸는 스위치
+		boolean flag = false;
+
+		// 한 단어 내의 index
+		int index = 0;
+
+		for (int i = 0; i < sb.length(); i++) {
+			char ch = sb.charAt(i);
+
+			// 공백이면 flag = false, index = 0으로 초기화.
+			if (ch == ' ') {
+				flag = false;
+				index = 0;
+			} else {
+				flag = true;
+			}
+
+			// flag가 true이면 ( 공백이 아니게 되면 )
+			if (flag) {
+
+				// index가 짝수일 경우 대문자로 바꾸기
+				if (index % 2 == 0) {
+					sb.setCharAt(i, (char) (ch - 32));
+				}
+
+				// index 하나씩 ++해주기
+				index++;
+			}
+		}
+
+		// String으로 변환하여 return
+		return sb.toString();
+	}
 }
+/*
+0.03 ~ 0.15ms
+
+테스트 1 〉통과 (0.04ms, 84.5MB)
+테스트 2 〉통과 (0.04ms, 89.5MB)
+테스트 3 〉통과 (0.03ms, 72.7MB)
+테스트 4 〉통과 (0.11ms, 82.3MB)
+테스트 5 〉통과 (0.07ms, 72.6MB)
+테스트 6 〉통과 (0.04ms, 77.7MB)
+테스트 7 〉통과 (0.05ms, 70.3MB)
+테스트 8 〉통과 (0.14ms, 73.8MB)
+테스트 9 〉통과 (0.07ms, 77.4MB)
+테스트 10 〉통과 (0.15ms, 75.7MB)
+테스트 11 〉통과 (0.11ms, 79.2MB)
+테스트 12 〉통과 (0.10ms, 83.3MB)
+테스트 13 〉통과 (0.06ms, 76.1MB)
+테스트 14 〉통과 (0.04ms, 76.3MB)
+테스트 15 〉통과 (0.06ms, 74.3MB)
+테스트 16 〉통과 (0.10ms, 72.6MB)
+ */
+
