dfb4cc5590250b924b2835acae6e5df98063281b
Haena,Cho <131599243+HaenaCho01@users.noreply.github.com>
2023-07-07 17:19:04 +0900

    Haena_P_이상한 문자 만들기,  예산 (#67)
    * Haena_P_이상한 문자 만들기
    
    * 풀이 과정
    1. 주어진 문자열을 StringBuilder로 만든다.
    2. 공백 위치를 -1로 설정한다.
    3. 문자열을 for문으로 돌며 공백의 위치를 찾고, 공백의 위치로부터 인덱스가 짝수인 경우 대문자, 홀수인 경우 소문자로 setCharAt과 toUpperCase, toLowerCase를 통해 변경 처리한다.
    4.만들어진 StringBuilder를 String으로 변환 후 리턴한다.
    
    * 테스트 통과 시간: 0.05ms ~ 0.28ms
    
    * Haena_P_예산
    
    * 풀이 과정
    1. 최대한 많은 부서를 지원할 수 있도록 필요 예산이 적은 부서부터 차례대로 정렬한다.(Arrays.sort())
    2. 지원한 금액과 부서를 계산하도록 변수를 선언한다.
    3. 향상된 for문을 통해 오름차순으로 정렬된 배열을 돌면서 부서에 금액을 지원하고 지원한 부서의 수를 카운트한다.
    4. 예산을 초과할 경우 부서의 수를 카운트하지 않고 멈춘다.
    
    * 테스트 통과 시간: 0.32ms ~ 0.61ms

--- src/Test07/Test0705_P/Haena.java
+++ src/Test07/Test0705_P/Haena.java
@@ -1,4 +1,52 @@
 package Test07.Test0705_P;
 
 public class Haena {
+	public static void main(String[] args) {
+		SolutionHn sol = new SolutionHn();
+		System.out.println(sol.solution("try hello world"));
+		System.out.println(sol.solution("  TRy HElLo  WORLD "));
+	}
 }
+
+class SolutionHn {
+	public String solution(String s) {
+		// 주어진 문자열을 StringBuilser로 만들기
+		StringBuilder answer = new StringBuilder(s);
+
+		int blankIndex = -1; // 공백 위치 (첫번째 글자를 고려하여 공백 위치를 -1부터 시작)
+
+		for (int i = 0; i < answer.length(); i++) {
+			// 공백 위치 찾기
+			if (answer.charAt(i) == ' ') {
+				blankIndex = i;
+			}
+
+			// 공백 위치 다음부터 인덱스가 짝수인 경우 대문자, 홀수인 경우 소문자로 처리하기
+			if (i == 0 || (i - blankIndex - 1) % 2 == 0) {
+				answer.setCharAt(i, Character.toUpperCase(answer.charAt(i)));
+			} else {
+				answer.setCharAt(i, Character.toLowerCase(answer.charAt(i)));
+			}
+		}
+		return answer.toString();
+	}
+}
+
+/*
+테스트 1 〉	통과 (0.11ms, 84.7MB)
+테스트 2 〉	통과 (0.07ms, 70.1MB)
+테스트 3 〉	통과 (0.05ms, 76.6MB)
+테스트 4 〉	통과 (0.28ms, 86.5MB)
+테스트 5 〉	통과 (0.26ms, 83.8MB)
+테스트 6 〉	통과 (0.05ms, 75MB)
+테스트 7 〉	통과 (0.06ms, 74.2MB)
+테스트 8 〉	통과 (0.23ms, 84.4MB)
+테스트 9 〉	통과 (0.07ms, 79.2MB)
+테스트 10 〉	통과 (0.20ms, 70.3MB)
+테스트 11 〉	통과 (0.23ms, 70.1MB)
+테스트 12 〉	통과 (0.21ms, 76.4MB)
+테스트 13 〉	통과 (0.16ms, 75.2MB)
+테스트 14 〉	통과 (0.07ms, 75.2MB)
+테스트 15 〉	통과 (0.14ms, 79.4MB)
+테스트 16 〉	통과 (0.15ms, 71.4MB)
+ */
