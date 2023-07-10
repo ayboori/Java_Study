4c20eaf9d05aa046fec8e1a30ff46d3290185a08
ayboori <105356296+ayboori@users.noreply.github.com>
2023-07-10 19:17:08 +0900

    Ayoung_P_예산 & 성격 유형 검사하기 (#73)
    * Ayoung_P_예산
    
    배열 내의 값들을 오름차순으로 정렬한다
    작은 값부터 하나씩 할당한다고 생각하며 예산에서 값을 뺀다.
    예산이 앞으로 뺄 값보다 적을 경우 지금까지 구한 값을 리턴한다.
    예산이 남았을 경우 부서 배열의 크기를 리턴한다. (모두에게 할당)
    
    * Ayoung_P_성격 유형 검사하기
    
    클래스 내에 지역 변수로 int 타입의 R,T,C,F,J,M,A,N 을 선언
    choices[] = servey[] 크기 만큼 루프를 돌며 내부 원소를 검사, scoring() 실시
    result() 를 통해 결과 리턴
    scoring()
    choice 의 값이 4 이상이면 1번째 char에 해당하는 변수에 + choice-4
    choice 의 값이 4 이하면 1번째 char에 해당하는 변수에 + 4-choice
    덧셈 연산은 oneElement()에서 수행
    
    oneElement()
    switch - case 문을 통해 연산 수행
    
    result()
    R / T 변수의 값들을 비교해 answer에 append
    
    이때 >= 을 사용해서 값이 동일할 경우 사전 상 앞에 있는 글자를 사용함
    
    💡 MBTI 값들을 객체로 만들까 했었는데, 실제 검사할 경우를 생각해보면 한 검사 당 solution 객체가 1개 생성된다. > 클래스 내에서는 공유해도 되겠다! > 지역 변수로 선언
    
    ---------
    
    Co-authored-by: JisooPyo <130378232+JisooPyo@users.noreply.github.com>

--- src/Test07/Test0707_P/Ayoung.java
+++ src/Test07/Test0707_P/Ayoung.java
@@ -1,4 +1,51 @@
 package Test07.Test0707_P;
-
+import java.util.Arrays;
 public class Ayoung {
+    public static void main(String[] args) {
+        Ayoung ay = new Ayoung();
+        System.out.println(new int[1, 3, 2, 5, 4],9);
+        System.out.println(new int[2, 2, 3, 3],10);
+    }
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d); // 오름차순 정렬
+
+        for(int i = 0 ; i < d.length ; i++){
+            if(budget < d[i]){
+                return i;
+                /* 할당 불가할 경우 지금까지 할당한 갯수 리턴
+                (이번 할당 전에 return 하므로 이번 루프 -1 값을 리턴해야 하는데,
+                i는 0부터 시작하므로 0을 리턴하면 된다.*/
+            }
+            budget -= d[i]; // 예산에서 할당한 값 빼기
+        }
+        return d.length;
+    }
 }
+
+/*
+0.32ms ~ 0.51ms
+
+테스트 1 〉	통과 (0.40ms, 77.7MB)
+테스트 2 〉	통과 (0.32ms, 65.3MB)
+테스트 3 〉	통과 (0.33ms, 74.4MB)
+테스트 4 〉	통과 (0.40ms, 71.5MB)
+테스트 5 〉	통과 (0.41ms, 74.4MB)
+테스트 6 〉	통과 (0.33ms, 73.5MB)
+테스트 7 〉	통과 (0.41ms, 74.6MB)
+테스트 8 〉	통과 (0.39ms, 72.3MB)
+테스트 9 〉	통과 (0.40ms, 76.4MB)
+테스트 10 〉	통과 (0.36ms, 73.9MB)
+테스트 11 〉	통과 (0.38ms, 75.4MB)
+테스트 12 〉	통과 (0.51ms, 77.8MB)
+테스트 13 〉	통과 (0.36ms, 74.8MB)
+테스트 14 〉	통과 (0.44ms, 81MB)
+테스트 15 〉	통과 (0.43ms, 66.9MB)
+테스트 16 〉	통과 (0.39ms, 74.2MB)
+테스트 17 〉	통과 (0.37ms, 78.1MB)
+테스트 18 〉	통과 (0.38ms, 74.1MB)
+테스트 19 〉	통과 (0.39ms, 78.1MB)
+테스트 20 〉	통과 (0.42ms, 70.2MB)
+테스트 21 〉	통과 (0.36ms, 73.3MB)
+테스트 22 〉	통과 (0.34ms, 74.9MB)
+테스트 23 〉	통과 (0.35ms, 75MB)
+ */
