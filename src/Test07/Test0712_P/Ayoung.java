c27d2d22b7041f0bbbe30afe8c614c0aceb1de74
ayboori <105356296+ayboori@users.noreply.github.com>
2023-07-12 19:26:47 +0900

    Ayoung_P_삼총사 (#75)
    * Ayoung_P_삼총사
    
    1. 경우의 수를 구하듯이 인덱스를 i, i+1, i+2로 증가시킨다
    2. i + (i+1) + (i+2) 가 0이 되는 경우, answer 의 값을 1 증가 시킨다.
    3. answer을 리턴한다.
    
    다른 사람의 풀이를 보니 첫번째 두번째 인덱스의 마지막 값에 -2, -1을 하는 게 좋았겠다!
    
    통과 시간 0.02ms ~ 0.03ms

--- src/Test07/Test0712_P/Ayoung.java
+++ src/Test07/Test0712_P/Ayoung.java
@@ -1,4 +1,41 @@
 package Test07.Test0712_P;
 
 public class Ayoung {
+    public static void main(String[] args) {
+        Ayoung ay = new Ayoung();
+
+        System.out.println(solution(new int[-2, 3, 0, 2, -5]); // 2
+        System.out.println(solution(new int[-3, -2, -1, 0, 1, 2, 3]); // 5
+        System.out.println(solution(new int[-1, 1, -1, 1]); // 0
+    }
+
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0 ; i < number.length ; i++){ // 첫 번째 인덱스
+            for (int j = i + 1; j < number.length ; j++){ // 두 번째 인덱스 - 1번 다음 값부터 증가
+                for (int k = j + 1 ; k < number.length ; k++){// 세 번째 인덱스 - 2번 다음 값부터 증가
+                    if(number[i] + number[j] + number[k] == 0){
+                        answer ++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
+
+/*
+테스트 1 〉	통과 (0.02ms, 71.4MB)
+테스트 2 〉	통과 (0.02ms, 76MB)
+테스트 3 〉	통과 (0.02ms, 74.5MB)
+테스트 4 〉	통과 (0.02ms, 76.5MB)
+테스트 5 〉	통과 (0.04ms, 78.4MB)
+테스트 6 〉	통과 (0.02ms, 76.9MB)
+테스트 7 〉	통과 (0.03ms, 75.4MB)
+테스트 8 〉	통과 (0.02ms, 72MB)
+테스트 9 〉	통과 (0.03ms, 78.1MB)
+테스트 10 〉	통과 (0.02ms, 69.4MB)
+테스트 11 〉	통과 (0.03ms, 77.3MB)
+테스트 12 〉	통과 (0.03ms, 75.2MB)
+테스트 13 〉	통과 (0.03ms, 77MB)
+ */
