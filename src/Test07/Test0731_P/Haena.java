6fbcfe66f855be60033331fe0eaf65443d944565
Haena,Cho <131599243+HaenaCho01@users.noreply.github.com>
2023-07-31 19:46:09 +0900

    Haena_P_두 개 뽑아서 더하기 (#114)
    * Haena_P_두 개 뽑아서 더하기
    
    1. 중복이 허용되지 않는 Set을 만든다.
    2. 배열을 돌며 서로 다른 인덱스의 값를 더한 값을 set에 추가한다.
    3. 만들어진 Set을 int 배열로 반환한다.
    4. int 배열을 정렬한 후 반환한다.
    
    *테스트 통과 시간: 2.92ms ~ 7.86ms
    
    * Haena_P_두 개 뽑아서 더하기 2
    
    1. 중복이 허용되지 않는 Set을 만든다.
    2. 배열을 돌며 서로 다른 인덱스의 값를 더한 값을 set에 추가한다.
    3. 만들어진 Set을 int 배열로 반환한다.
    4. int 배열을 정렬한 후 반환한다.
    
    *테스트 통과 시간: 0.41ms ~ 4.57ms
    
    * Haena_P_두 개 뽑아서 더하기 주석 추가
    
    * Haena_P_두 개 뽑아서 더하기 3
    
    1. 중복이 허용되지 않는 Set을 만든다.
    2. 배열을 돌며 서로 다른 인덱스의 값를 더한 값을 set에 추가한다.
    3. 만들어진 Set을 int 배열로 반환한다.
    4. int 배열을 정렬한 후 반환한다.
    
    *테스트 통과 시간: 0.11ms ~ 1.89ms
    
    * Haena_P_두 개 뽑아서 더하기 다른 사람의 풀이
    
    1. 중복이 허용되지 않는 Set을 만든다.
    2. 배열을 돌며 서로 다른 인덱스의 값를 더한 값을 set에 추가한다.
    3. 만들어진 Set을 int 배열로 정렬한 후 반환한다.
    
    *테스트 통과 시간: 2.26ms ~ 10.30ms
    
    * Haena_P_두 개 뽑아서 더하기 코드 개선
    
    * Haena_P_두 개 뽑아서 더하기 테스트 통과 시간 변경
    
    * Haena_P_두 개 뽑아서 더하기 테스트 통과 시간 변경

--- src/Test07/Test0731_P/Haena.java
+++ src/Test07/Test0731_P/Haena.java
@@ -1,5 +1,43 @@
 // 두 개 뽑아서 더하기
 package Test07.Test0731_P;
 
+import java.util.*;
+
 public class Haena {
+	public int[] solution(int[] numbers) {
+		// 1. 중복이 허용되지 않는 Set을 만든다.
+		Set<Integer> answerSet = new HashSet<>();
+
+		// 2. 배열을 돌며 서로 다른 인덱스의 값를 더한 값을 set에 추가한다.
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i+1; j < numbers.length; j++) {
+				answerSet.add(numbers[i] + numbers[j]);
+			}
+		}
+
+		// 3. 만들어진 Set을 int 배열로 반환한다.
+		int[] answer = Arrays.stream(answerSet.toArray(new Integer[0])).mapToInt(Integer::intValue).toArray();
+
+		// 4. int 배열을 정렬한 후 반환한다.
+		Arrays.sort(answer);
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Haena sol = new Haena();
+		System.out.println(Arrays.toString(sol.solution(new int[]{2, 1, 3, 4, 1})));
+		System.out.println(Arrays.toString(sol.solution(new int[]{5, 0, 2, 7})));
+	}
 }
+
+/*
+테스트 1 〉	통과 (2.32ms, 77.4MB)
+테스트 2 〉	통과 (3.17ms, 76.1MB)
+테스트 3 〉	통과 (2.38ms, 76.2MB)
+테스트 4 〉	통과 (3.31ms, 77.3MB)
+테스트 5 〉	통과 (2.34ms, 74.1MB)
+테스트 6 〉	통과 (2.62ms, 75.6MB)
+테스트 7 〉	통과 (3.20ms, 79.1MB)
+테스트 8 〉	통과 (3.46ms, 72.4MB)
+테스트 9 〉	통과 (3.30ms, 77.5MB)
+ */
